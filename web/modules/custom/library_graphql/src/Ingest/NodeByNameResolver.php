a9a47a760cd522623e80f212093576dd2f844c2e
Mehmet <mehmetgokgul.ybu@gmail.com>
2026-07-24 19:22:19 +0300

    feat: find or create author/director nodes by name for JSON import

---
+++ web/modules/custom/library_graphql/src/Ingest/NodeByNameResolver.php
@@ -0,0 +1,49 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Drupal\library_graphql\Ingest;
+
+use Drupal\Core\Entity\EntityTypeManagerInterface;
+use Drupal\node\NodeInterface;
+
+class NodeByNameResolver {
+
+    public function __construct(protected EntityTypeManagerInterface $entityTypeManager) {}
+
+    public function findOrCreate(string $bundle, string $name): ?NodeInterface {
+
+        $name = trim($name);
+
+        if ($name === '') {
+            return NULL;
+        }
+
+        $storage = $this->entityTypeManager->getStorage('node');
+
+        $ids = $storage->getQuery()
+            ->condition('type', $bundle)
+            ->condition('title', $name)
+            ->accessCheck(FALSE)
+            ->range(0, 1)
+            ->execute();
+
+        if (!empty($ids)) {
+            $node_id = reset($ids);
+            return $storage->load($node_id);
+        }
+
+        $node = $storage->create([
+            'type' => $bundle,
+            'title' => $name,
+        ]);
+
+        $violations = $node->validate();
+        if ($violations->count() > 0) {
+            return NULL;
+        }
+
+        $node->save();
+        return $node;
+    }
+}
